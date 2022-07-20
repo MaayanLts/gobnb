600c0485878cf4a67301f61f74fceb3b99d69740
DavidLewensohn <davidlewensohn@gmail.com>
2022-07-20 15:28:42 +0300

    create store

---
+++ frontend/src/store/modules/stay-store.js
@@ -0,0 +1,17 @@
+// import { stayService } from '...'
+
+
+export default {
+  state: {
+ 
+  },
+  getters: {
+
+  },
+  mutations: {
+  },
+  actions: {
+   
+  },
+  modules: {},
+}
---
+++ frontend/src/views/stayDetails.page.vue
@@ -0,0 +1,9 @@
+<template>
+<h3>hi from details</h3>
+</template>
+
+<script >
+  
+
+
+</script>
