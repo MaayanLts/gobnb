2826b5c9e9b7653eaf04eaf5fc930ab63834b116
DavidLewensohn <davidlewensohn@gmail.com>
2022-07-21 01:00:49 +0300

    remove old imges and css for detalis

--- frontend/src/views/stay-details.vue
+++ frontend/src/views/stay-details.vue
@@ -1,32 +1,46 @@
-<template>
-    <h2>{{ stay.name }}</h2>
-    <div class="limks-line">
-        <a href="">{{ stay.loc.country }}</a>
-        <a href="">⇧ Share</a>
-        <a href="">♡ Save</a>
-    </div>
-    <div class="images-holder">
-        <img v-for="url in stay.imgUrls" :src=url alt="">
-    </div>
+<template >
+    <section class="main-details-container">
 
-    <div class="hosted-by-txt">
-        <h2>{{ stay.name }} hosted by {{ stay.host.fullname }}</h2>
-        <h3>{{ stay.capacity }} guests</h3>
-    </div>
-    <img class="host-img" :src="stay.host.imgUrl" alt="">
+        <h2>{{ stay.name }}</h2>
+        <div class="limks-line">
+            <div class="start-links">
+                <a href="">★ {{ meanRate }}</a>
+                <a href="">★ {{ reviews }} reviews</a>
+                <a href=""> {{ stay.loc.city }}</a>
+                <a href=""> {{ stay.loc.country }}</a>
+            </div>
+
+            <div class="end-links">
+                <a href="">⇧ Share</a>
+                <a href="">♡ Save</a>
+            </div>
+        </div>
+
+        <div class="images-holder">
+            <img v-for="url in stay.imgUrls" :src=url alt="">
+        </div>
+
+        <div class="hosted-by-txt">
+            <h2>{{ stay.name }} hosted by {{ stay.host.fullname }}</h2>
+            <h3>{{ stay.capacity }} guests</h3>
+        </div>
+        <img class="host-img" :src="stay.host.imgUrl" alt="">
 
 
-    <p class="summary">{{ stay.summary }}</p>
+        <p class="summary">{{ stay.summary }}</p>
 
-    <div class="amenities">
-        <h2>What this place offers</h2>
-        <ul><li v-for="ament in stay.amenities">{{ament}}</li></ul>
-    </div>
+        <div class="amenities">
+            <h2>What this place offers</h2>
+            <ul>
+                <li v-for="ament in stay.amenities">{{ ament }}</li>
+            </ul>
+        </div>
 
-    <div class="reviews">
-        <h2>★ {{stay.reviews.rate}}</h2>
-    
-    </div>
+        <div class="reviews">
+            <h2>★ {{ meanRate }}</h2>
+
+        </div>
+    </section>
 
 </template>
 
@@ -38,7 +52,7 @@ export default {
                 "_id": "10006547",
                 "name": "Ribeira Charming Duplex",
                 "type": "House",
-                "imgUrls": ["https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large", "otherImg.jpg"],
+                "imgUrls": ["https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large", "https://a0.muscache.com/im/users/31635864/profile_pic/1429604852/original.jpg?aki_policy=profile_x_medium", "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large", "https://a0.muscache.com/im/users/31635864/profile_pic/1429604852/original.jpg?aki_policy=profile_x_medium", "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large"],
                 "price": 80.00,
                 "summary": "Fantastic duplex apartment with three bedrooms, located in the historic area of Porto, Ribeira (Cube)...",
                 "capacity": 8,
@@ -74,24 +88,44 @@ export default {
                             "fullname": "user2",
                             "imgUrl": "/img/img2.jpg"
                         }
+                    },
+                    {
+                        "id": "gfdgfds",
+                        "txt": "nice hosts. Cooked traditional...",
+                        "rate": 5,
+                        "by": {
+                            "_id": "u103",
+                            "fullname": "user2",
+                            "imgUrl": "/img/img2.jpg"
+                        }
                     }
                 ],
                 "likedByUsers": ['mini-user'] // for user-wishlist : use $in
             }
         }
     },
+    methods: {
+
+    },
     components: {
     },
     computed: {
+        meanRate() {
+            var sum = 0
+            const reviews = this.stay.reviews
+            console.log(reviews)
+            reviews.forEach((rev) => { sum += rev.rate });
+            return sum / reviews.length
+        },
+        reviews() {
+            const reviews = this.stay.reviews
+            return reviews.length
+        }
     },
     created() { },
 }
 </script>
 
 <style>
-.limks-line {
-    display: flex;
-    justify-content: space-between;
 
-}
 </style>
