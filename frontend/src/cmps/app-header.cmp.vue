dd634cb58242995931b9ba7db82d580353f988b6
Maayan <millermaayan1.com>
2022-07-21 18:30:11 +0300

    serch logo

--- frontend/src/cmps/app-header.cmp.vue
+++ frontend/src/cmps/app-header.cmp.vue
@@ -41,7 +41,20 @@
           <li class="li-header"><a href="#">Anywhere</a></li>
           <li class="li-header"><a href="#">Anyweek</a></li>
           <li class="li-header"><a href="#">add gust</a>
-            <img class="search-icon" src="/src/assets/logo/serch.png" alt="">
+            <div class="search-icon-container">
+              <img class="search-icon" src="../assets/logo/serch_icon.png"
+                alt="">
+              <!-- <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true" role="presentation" focusable="false"
+                style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 5.33333; overflow: visible;">
+                <g fill="none">
+                  <path
+                    d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9">
+                  </path>
+                </g>
+              </svg> -->
+            </div>
+
           </li>
 
         </ul>
@@ -54,7 +67,8 @@
       <a>swith to hosting</a>
       <img class="globe-icon" src="/src/assets/logo/Globe_icon.svg" alt="">
       <div class="user-humburger">
-        <i>≡</i>
+        <img class="burger-icon" src="/src/assets/logo/Hamburger_icon.svg"
+          alt="">
         <img class="user-img" src="/src/assets/logo/user.jpg" alt="">
       </div>
     </div>
