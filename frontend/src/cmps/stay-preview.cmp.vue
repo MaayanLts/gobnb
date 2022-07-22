83641bee2efaf947e4913178d35d50b329dfcbc7
Maayan <millermaayan1.com>
2022-07-22 15:12:10 +0300

    CRUSS

--- frontend/src/cmps/stay-preview.cmp.vue
+++ frontend/src/cmps/stay-preview.cmp.vue
@@ -1,11 +1,18 @@
 <template>
   <section class="stay-preview" @click="showStayDetails">
-  <img class="preview-img-carusela" :src="imgUrl" /> 
-  <span class="bold">{{ stay.name }}</span><span>{{ rating  }}</span><br/>
-  <span>{{ stay.propertyType }} • {{ stay.roomType }}</span><br/>
-  <span>{{ stay.beds }} beds • {{ stay.bedrooms }} bedrooms</span><br/>
-  <span class="bold">${{ stay.price }} night</span><br/>
-  
+
+
+
+    <el-carousel trigger="click" interval="0">
+      <el-carousel-item v-for="(img, idx) in stay.imgUrls" :key="img">
+        <img class="preview-img-carusela" :src="imgUrl + img" />
+      </el-carousel-item>
+    </el-carousel>
+
+    <span class="bold">{{ stay.name }}</span><span>{{ rating }}</span><br />
+    <span>{{ stay.propertyType }} • {{ stay.roomType }}</span><br />
+    <span>{{ stay.beds }} beds • {{ stay.bedrooms }} bedrooms</span><br />
+    <span class="bold">${{ stay.price }} night</span><br /> -->
   </section>
 </template>
 
@@ -13,7 +20,7 @@
 import slide from './slide-cmp.vue';
 
 export default {
-  name:'StayPrevew',
+  name: 'StayPrevew',
   props: {
     stay: {
       type: Object,
@@ -29,21 +36,43 @@ export default {
     slide,
   },
   computed: {
-   imgUrl() {
-      return `src/images/${this.stay.imgUrls[0]}`
-   },
-   rating(){
-      return this.stay.reviewScores.rating /20
-   }
+    imgUrl() {
+      return `src/images/`
+    },
+    rating() {
+      return this.stay.reviewScores.rating / 20
+    }
   },
-  created(){
+  created() {
   },
   unmounted() {
   },
   methods: {
     showStayDetails() {
-    //  this.$router.push("/stay/"+ this.stay._id)
+      //  this.$router.push("/stay/"+ this.stay._id)
     },
   },
+
 };
 </script>
+
+  
+
+
+<style scoped>
+.el-carousel__item h3 {
+  display: flex;
+  color: #475669;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
