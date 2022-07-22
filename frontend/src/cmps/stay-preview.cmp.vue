2f3596078ebb628f3c46912bff01a8e25df6995e
Maayan <maayan.loutset@gmail.com>
2022-07-22 20:00:33 +0300

    Add heart& stay info

--- frontend/src/cmps/stay-preview.cmp.vue
+++ frontend/src/cmps/stay-preview.cmp.vue
@@ -1,8 +1,11 @@
 <template>
   <section class="stay-preview" @click.stop = "showStayDetails">
-
+  
     <el-carousel trigger="click" :autoplay="false">
       <el-carousel-item v-for="(img) in stay.imgUrls" :key="img">
+        <svg class="svg-heart" style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"></path>
+        </svg>
         <img class="preview-img-carusela" :src="imgUrl + img" />
       </el-carousel-item>
     </el-carousel>
@@ -53,7 +56,7 @@ export default {
           if(this.stay.address.country) 
             propertyDesc += `, `
         }
-        
+
         if(this.stay.address.country) 
           propertyDesc += `${this.stay.address.country}`
 
