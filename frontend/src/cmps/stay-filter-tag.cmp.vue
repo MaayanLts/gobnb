7a4500c4036f82eb621a64028f144485f6eaf844
Maayan <maayan.loutset@gmail.com>
2022-07-23 20:44:48 +0300

    Change tags images& add tags to header

--- frontend/src/cmps/stay-filter-tag.cmp.vue
+++ frontend/src/cmps/stay-filter-tag.cmp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags-container">
     <div class="tag-card" v-for="tag in tags" :key="tag" @click="tagSelect(tag)">
-      <div><img class="img" :src="tagImage" /></div>
+      <div><img class="img" :src="tagImage(tag)" /></div>
       <div>{{ tag }}</div>
     </div>
   </div>
@@ -16,10 +16,16 @@ export default {
   },
    data() {
     return {
-      tags: ["island", "lake", "snow", "city", "camping"],
+      tags: ["Islands", 'Beach', 'Amazing pools', 'OMG', 'National parks', 
+             'Cabins', 'Design', "Camping", 'Amazing views', 'Desert','Arctic', 
+             'Tiny homes', 'Lakefront', 'Surfing', 'Shared homes', 'Caves', 'Tropical', 'Bed & breakfasts', 'Earth homes',
+             'Iconic cities','Countryside','Luxe','Lake','A-frames','Castles','Farms','historical-homes',"City", "Boat"],
     }
   },
   methods: {
+    tagImage(tag) {
+      return `/src/assets/fonts/airbnb-label-imgs/${tag.replace(' & ','-').replace(' ','-').toLowerCase()}.jpg`
+    },
     tagSelect(tag) {
       this.$store.dispatch({
         type: 'setFilterBy',
@@ -28,9 +34,6 @@ export default {
     },
   },
   computed: {
-    tagImage() {
-      return `/src/assets/fonts/airbnb-label-imgs/island.jpg`
-    }
   },
   created() {
 
