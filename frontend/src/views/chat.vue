66c06824344ac34d314aad59638a8c7e691828be
DavidLewensohn <davidlewensohn@gmail.com>
2022-07-27 18:04:09 +0300

    chat for host and client

--- frontend/src/views/chat.vue
+++ frontend/src/views/chat.vue
@@ -1,34 +1,41 @@
 <template>
   <div class="container">
-    <h1>Host name</h1>
-    <h2>Lets Chat About gobnb... </h2>
-    
+    <h1>gobnb</h1>
+    <h4 v-if="hostName === user.fullname">Hello {{ hostName }}, you have a new message </h4>
+    <h4 v-else>write to {{ hostName }}</h4>
     <ul>
       <li v-for="(msg, idx) in msgs" :key="idx">
-        <span>{{msg.from}}:</span>{{msg.txt}}
+        <span>{{ msg.from }}:</span>{{ msg.txt }}
       </li>
     </ul>
     <hr />
     <form @submit.prevent="sendMsg">
-      <input type="text" v-model="msg.txt" placeholder="Your msg"/>
+      <input type="text" v-model="msg.txt" placeholder="Your msg" />
       <button>Send</button>
     </form>
   </div>
 </template>
 
 <script>
-import {socketService, SOCKET_EMIT_SEND_MSG, SOCKET_EVENT_ADD_MSG, SOCKET_EMIT_SET_TOPIC} from '../services/socket.service' ///
+import { socketService, SOCKET_EMIT_SEND_MSG, SOCKET_EVENT_ADD_MSG, SOCKET_EMIT_SET_TOPIC } from '../services/socket.service'
 
 export default {
   data() {
     return {
-      msg: {from: 'Guest', txt: ''},
+      msg: { from: 'Guest', txt: '' },
       msgs: [],
+      hostName: null,
+      user:null,
+
     }
   },
   created() {
-    socketService.setup()
-    socketService.emit(SOCKET_EMIT_SET_TOPIC, 'airbnb') 
+    this.user = userService.getLoggedinUser()
+    this.hostName = this.$route.params.name
+    console.log(this.user)    
+    console.log(this.hostName)
+    // socketService.setup()
+    socketService.emit(SOCKET_EMIT_SET_TOPIC, this.hostName)
     socketService.on(SOCKET_EVENT_ADD_MSG, this.addMsg)
   },
   destroyed() {
@@ -44,13 +51,14 @@ export default {
       // TODO: next line not needed after connecting to backend
       // this.addMsg(this.msg)
       // setTimeout(()=>this.addMsg({from: 'Dummy', txt: 'Yey'}), 2000)
-      
-      const user = userService.getLoggedinUser()   ///
+      const user = userService.getLoggedinUser()
       const from = (user && user.fullname) || 'Guest'
       this.msg.from = from
-      console.log('msg;',this.msg);
       socketService.emit(SOCKET_EMIT_SEND_MSG, this.msg)
-      this.msg = {from, txt: ''}
+      this.msg = { from, txt: '' }
+    },
+    changeTopic() {
+      socketService.emit(SOCKET_EMIT_SET_TOPIC, this.hostName)
     }
   }
 }
