f540ffc44613c19c206aff1efbc318fc9af1a73e
Maayan <maayan.loutset@gmail.com>
2022-07-30 03:06:45 +0300

    bckoffice

--- frontend/src/store/modules/order-store.js
+++ frontend/src/store/modules/order-store.js
@@ -13,18 +13,25 @@ export default {
 		loadOrders(state) {
 			state.orders = orderService.query()
 		},
-		approveOrder(state, { orderId }){
+		// approveOrder(state, { orderId }){
+		// 	let order = state.orders.find(order => order._id === orderId)
+		// 	order.orderStatus = "approved"
+
+		// 	orderService.save(state.orders)
+		// },
+		// declineOrder(state, { orderId }){
+		// 	let order = state.orders.find(order => order._id === orderId)
+		// 	order.orderStatus = "declined"
+
+		// 	orderService.save(state.orders)
+		// },
+		changeOrderOrder(state, { orderId, status }){
 			let order = state.orders.find(order => order._id === orderId)
-			order.orderStatus = "approved"
+			order.orderStatus = status
 
 			orderService.save(state.orders)
 		},
-		declineOrder(state, { orderId }){
-			let orderIndex = state.orders.findIndex(order => order._id === orderId)
-			state.orders.splice(orderIndex, 1)
-
-			orderService.save(state.orders)
-		},
+		
 	},
 
 	modules: {},
