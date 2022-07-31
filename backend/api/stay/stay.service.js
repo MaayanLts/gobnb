7cfd36f02f94a46e64c767c4c53314d6b5396f41
Maayan <millermaayan1.com>
2022-07-31 03:08:40 +0300

    back filter

--- backend/api/stay/stay.service.js
+++ backend/api/stay/stay.service.js
@@ -5,138 +5,121 @@ const asyncLocalStorage = require('../../services/als.service')
 const fs = require('fs')
 
 async function query(filterBy = {}) {
-    try {
-        const criteria = _buildCriteria(filterBy)
-        const collection = await dbService.getCollection('stay')
-        const stays = await collection.find(criteria).toArray()
-        // var stays = await collection.aggregate([
-        //     {
-        //         $match: criteria
-        //     },
-        //     {
-        //         $lookup:
-        //         {
-        //             localField: 'byUserId',
-        //             from: 'user',
-        //             foreignField: '_id',
-        //             as: 'byUser'
-        //         }
-        //     },
-        //     {
-        //         $unwind: '$byUser'
-        //     },
-        //     {
-        //         $lookup:
-        //         {
-        //             localField: 'aboutUserId',
-        //             from: 'user',
-        //             foreignField: '_id',
-        //             as: 'aboutUser'
-        //         }
-        //     },
-        //     {
-        //         $unwind: '$aboutUser'
-        //     }
-        // ]).toArray()
-        // stays = stays.map(stay => {
-        //     stay.byUser = { _id: stay.byUser._id, fullname: stay.byUser.fullname }
-        //     stay.aboutUser = { _id: stay.aboutUser._id, fullname: stay.aboutUser.fullname }
-        //     delete stay.byUserId
-        //     delete stay.aboutUserId
-        //     return stay
-        // })
-        return stays
-    } catch (err) {
-        logger.error('cannot find stays', err)
-        throw err
-    }
+	try {
+		const criteria = _buildCriteria(filterBy)
+		const collection = await dbService.getCollection('stay')
+		const stays = await collection.find(criteria).toArray()
+		console.log('stays:', stays.length)
 
+		return stays
+	} catch (err) {
+		logger.error('cannot find stays', err)
+		throw err
+	}
 }
 
 async function remove(stayId) {
-    try {
-        // const store = asyncLocalStorage.getStore()
-        // const { loggedinUser } = store
-        const collection = await dbService.getCollection('stay')
-        // remove only if user is owner/admin
-        const criteria = { _id: ObjectId(stayId) }                    
-        // if (!loggedinUser.isAdmin) criteria.byUserId = ObjectId(loggedinUser._id)
-        const { deletedCount } = await collection.deleteOne(criteria)
-        console.log('dbug: stayId.ser:', deletedCount )
-        return deletedCount
-    } catch (err) {
-        logger.error(`cannot remove stay ${stayId}`, err)
-        throw err
-    }
+	try {
+		const collection = await dbService.getCollection('stay')
+		const criteria = {_id: ObjectId(stayId)}
+		const {deletedCount} = await collection.deleteOne(criteria)
+		console.log('dbug: stayId.ser:', deletedCount)
+		return deletedCount
+	} catch (err) {
+		logger.error(`cannot remove stay ${stayId}`, err)
+		throw err
+	}
 }
 
-
 async function add(stay) {
-    try {
-        const collection = await dbService.getCollection('stay')
-        const addedStay = await collection.insertOne(stay)
-        return stay
-    } catch (err) {
-        logger.error('cannot insert stay', err)
-        throw err
-    }
+	try {
+		const collection = await dbService.getCollection('stay')
+		const addedStay = await collection.insertOne(stay)
+		return stay
+	} catch (err) {
+		logger.error('cannot insert stay', err)
+		throw err
+	}
 }
 
 function _buildCriteria(filterBy) {
-    const criteria = {}
-    if (filterBy.byUserId) criteria.byUserId = filterBy.byUserId
-    return criteria
+	console.log('filterBy:', filterBy)
+	var criteria = {}
+
+	if (filterBy.country) {
+		var txtCriteria = {$regex: filterBy.country, $options: 'i'}
+		criteria = {
+			$or: [
+				{'address.country': txtCriteria},
+				{'address.city': txtCriteria},
+				{'address.street': txtCriteria},
+			],
+		}
+	}
+
+	if (filterBy.tag) {
+		var txtCriteria = {$regex: filterBy.tag, $options: 'i'}
+		criteria = {tags: txtCriteria}
+	}
+	if (filterBy.byAmenities) {
+		var amenities = []
+		for (let i = 0; i < filterBy.byAmenities.length; i++) {
+			amenities.push({amenities: {$regex: filterBy.byAmenities[i], $options: 'i'}})
+		}
+		criteria = {$or: amenities}
+	}
+	if (filterBy.byPrice) {
+		criteria = {price: {$gte: +filterBy.byPrice[0], $lte: +filterBy.byPrice[1]}}
+	}
+	return criteria
 }
 
 async function getById(stayId) {
-    try {
-        const collection = await dbService.getCollection('stay')
-        const stay = collection.findOne({ _id: ObjectId(stayId) })
-        return stay
-    } catch (err) {
-        logger.error(`while finding stay ${stayId}`, err)
-        throw err
-    }
+	try {
+		const collection = await dbService.getCollection('stay')
+		const stay = collection.findOne({_id: ObjectId(stayId)})
+		return stay
+	} catch (err) {
+		logger.error(`while finding stay ${stayId}`, err)
+		throw err
+	}
 }
 
 async function update(stay) {
-    try {
-        var id = ObjectId(stay._id)
-        delete stay._id
-        const collection = await dbService.getCollection('stay')
-        await collection.updateOne({ _id: id }, { $set: { ...stay } })
-        stay._id = id
-        return stay
-    } catch (err) {
-        logger.error(`cannot update stay ${stayId}`, err)
-        throw err
-    }
+	try {
+		var id = ObjectId(stay._id)
+		delete stay._id
+		const collection = await dbService.getCollection('stay')
+		await collection.updateOne({_id: id}, {$set: {...stay}})
+		stay._id = id
+		return stay
+	} catch (err) {
+		logger.error(`cannot update stay ${stayId}`, err)
+		throw err
+	}
 }
 
 module.exports = {
-    query,
-    remove,
-    add,
-    update,
-    getById
+	query,
+	remove,
+	add,
+	update,
+	getById,
 }
 
 // _saveStays()
-async function _saveStays(){
-    const stays = require('../../services/stay.json')
+async function _saveStays() {
+	const stays = require('../../services/stay.json')
 
-    try {
-        console.log(1, stays[188])
-        const collection = await dbService.getCollection('stay')
-        stays.forEach((stay) => {
-             stay._id = ObjectId(stay._id )   
-             add(stay)
-        });
-        
-    } catch (err) {
-        logger.error('cannot insert stays', err)
-        throw err
-    }
-
+	try {
+		console.log(1, stays[188])
+		const collection = await dbService.getCollection('stay')
+		stays.forEach((stay) => {
+			stay._id = ObjectId(stay._id)
+			add(stay)
+		})
+	} catch (err) {
+		logger.error('cannot insert stays', err)
+		throw err
+	}
 }
-
